a43bf51e8cc0a43f80639052a3b08d573fe5677a
yolokbrother <www.mineman198@gmail.com>
2023-05-29 03:14:30 +0800

    tsx

--- my-next-ts-app/pages/index.tsx
+++ my-next-ts-app/pages/index.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Link from 'next/link';
-import ResponsiveAppbar from "../components/index/responsiveAppbar.jsx";
+import ResponsiveAppbar from "../components/index/responsiveAppbar.tsx";
 
 const IndexPage = () => (
 <>
